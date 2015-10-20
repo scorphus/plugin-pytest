85fdd43c91f2f87188cb1178079d2c830336c094
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-19 23:36:02 -0200

    refact(pytest-cov-html): ask whether to open results or not

--- pytest-cov-html.fish
+++ pytest-cov-html.fish
@@ -9,6 +9,7 @@ function pytest-cov-html -d 'Run py.test missing with html coverage'
     set exit_code $status
     test $exit_code -eq 2; \
         and echo "Tip: did you `pip install pytest-cov`?"; and return $exit_code
-    open htmlcov/index.html
+    read -p "echo 'Open HTML coverage results page? (y/N) '" doit
+    contains "$doit" "y" "Y"; and open htmlcov/index.html
     return $exit_code
 end
